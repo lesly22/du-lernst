bb2bb9d353717c618aa8964f9e306e3bb4fbe2d1
BorisA3 <botisalarcon@gmail.com>
2020-10-30 15:40:45 -0600

    Creacion e implementacion del archivo de rutas para el servidor

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
-// express
+// Importar los modulos requeridos para el funcionamiento del servidor
 const express = require("express");
-// Importar handlebars como template engine
 const exphbs = require("express-handlebars");
-// Importar body parser que nos permite acceder al cuerpo de la petición HTTP
-const bodyParser = require("body-parser");
+const router = require("./routes/index");
 
+// Habilitar el archivo de variables de entorno
 require("dotenv").config({ path: ".env" });
 
 // Crear un servidor express
 const app = express();
 
 // Indicar a express utilizar handlebars como template engine
-app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+//app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 
-app.set("view engine", "hbs");
+//app.set("view engine", "hbs");
 
 // Habilitar body parse para leer los datos del cuerpo de peticiones POST
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ extended: true }));
 
-// Crear una ruta para /
-// Información sobre los verbos HTTP
-// https://developer.mozilla.org/es/docs/Web/HTTP/Methods
-app.use("/", (req, res, next) => {
-  res.send("Du Lernst");
-});
+// Implementar nuestero router
+app.use("/",router());
 
 // Inicializar el servidor en un puerto en específico
 app.listen(process.env.PORT);
