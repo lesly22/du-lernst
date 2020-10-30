44d4862f939ce0707b992cde2aafd779c66aa993
BorisA3 <botisalarcon@gmail.com>
2020-10-30 17:02:19 -0600

    videoModelo

---
+++ models/Video.js
@@ -0,0 +1,23 @@
+// Importar los modulos requeridos
+const mongoose = require("mongoose");
+
+// Definicion del shema
+const videoSchema = new mongoose.Schema({
+    url: {
+        type : String,
+        unique: true,
+        trim: true,
+        required: true
+    },
+    nombreVideo: {
+        type: String,
+        required: true,
+        trim: false
+    },
+    descripcion: {
+        type: String,
+        require: true
+    }      
+});
+
+module.exports = mongoose.model("Usuarios", usuarioSchema);
