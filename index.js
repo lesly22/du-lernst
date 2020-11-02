ef94d2a0908acf3dcf5d0983349abe7168d96fb9
BorisA3 <botisalarcon@gmail.com>
2020-11-01 19:21:26 -0600

    Regristar usuario a la DB mg e inst. de modulos.

--- index.js
+++ index.js
@@ -1,31 +1,61 @@
-// Importar los modulos requeridos para el funcionamiento del servidor
+// Importar los módulos requeridos para el funcionamiento del servidor
+const mongoose = require("mongoose");
 const express = require("express");
 require("./config/db");
 const exphbs = require("express-handlebars");
 const router = require("./routes/index");
+const bodyParser = require("body-parser");
+const path = require("path");
+const passport = require("./config/passport");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const flash = require("connect-flash");
 
 // Habilitar el archivo de variables de entorno
 require("dotenv").config({ path: ".env" });
 
-// Crear un servidor express
+// Crear un servidor utilizando express
 const app = express();
 
-// Indicar a express utilizar handlebars como template engine
+// Habilitar Handlebars como nuestro template engine
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 
 app.set("view engine", "hbs");
 
-// Habilitar body parse para leer los datos del cuerpo de peticiones POST
-//app.use(bodyParser.urlencoded({ extended: true }));
+// Definir ruta para archivos estáticos.
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res, next) => {
-    res.render("home");
-  });
+// Crear la sesión de usuario y la cookie encargada de almacenarla
+app.use(cookieParser('hola'));
 
-// Implementar nuestero router
-app.use("/",router());
+app.use(
+  session({
+    secret: process.env.SECRET,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
 
-// Inicializar el servidor en un puerto en específico
+// Habilitar passport y la estrategia local
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Habilitar los mensajes flash
+app.use(flash());
+
+// Midleware personalizado para agregar mensajes flash
+app.use((req, res, next) => {
+  res.locals.messages = req.flash();
+  next();
+});
+
+// Habilitar body-parser para obtener el cuerpo de la petición
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Implementar nuestro router
+app.use("/", router());
+
 app.listen(process.env.PORT);
-
-//
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,55 @@
+// Importar los módulos requeridos
 const express = require("express");
+const usuarioController = require("../controllers/usuarioController");
+const authController = require("../controllers/authController");
+const { check } = require("express-validator");
 
+// Configura y mantiene todos los endpoints en el servidor
 const router = express.Router();
 
 module.exports = () => {
-    router.get("/", (req, res, next)=> {
-        res.send("Du Lernst");
-    });
-    return router;
+  // Rutas disponibles
+  router.get("/", (req, res, next) => {
+    res.send("¡Bienvenido a Cashize!");
+  });
+
+  // Rutas para usuario
+  router.get("/crear-cuenta", usuarioController.formularioCrearCuenta);
+
+  router.post(
+    "/crear-cuenta",
+    [
+      // Realizar una verificación de los atributos del formulario
+      // https://express-validator.github.io/docs/index.html
+      check("nombre", "Debes ingresar tu nombre completo.")
+        .not()
+        .isEmpty()
+        .escape(),
+      check("email", "Debes ingresar un correo electrónico.").not().isEmpty(),
+      check("email", "El correo electrónico ingresado no es válido.")
+        .isEmail()
+        .normalizeEmail(),
+      check("password", "Debes ingresar una contraseña").not().isEmpty(),
+    ],
+    usuarioController.crearCuenta
+  );
+
+  router.get("/iniciar-sesion", usuarioController.formularioIniciarSesion);
+
+  router.post("/iniciar-sesion", authController.autenticarUsuario);
+
+  router.get("/olvide-password", authController.formularioRestablecerPassword);
+
+  router.post("/olvide-password", authController.enviarToken);
+
+  router.get("/olvide-password/:token", authController.formularioNuevoPassword);
+
+  router.post("/olvide-password/:token", authController.almacenarNuevaPassword);
+
+  // Rutas de administración
+  router.get("/administrar", (req, res, next) => {
+    res.send("Administración del sitio");
+  });
+
+  return router;
 };
